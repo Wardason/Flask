1db52e34e48be84b263870ab6e2a44f9d44f034b
Leonard <66140015+Wardason@users.noreply.github.com>
2021-05-05 21:41:05 +0200

    usd converter function

--- files/currency.py
+++ files/currency.py
@@ -1,6 +1,14 @@
 from flask import Flask, Blueprint, render_template, request
+from forex_python.converter import CurrencyRates
 
 currency = Blueprint('currency', __name__)
+
+c = CurrencyRates()
+usd = c.get_rate('EUR', 'USD')
+pound = c.get_rate('EUR', 'GBP')
+yen = c.get_rate('EUR', 'JPY')
+franc = c.get_rate('EUR', 'CHF')
+yuan = c.get_rate('EUR', 'CNY')
 
 
 @currency.route('/currency', methods=['GET', 'POST'])
@@ -8,14 +16,30 @@ def convert():
 
     if request.method == 'POST':
         number_input = request.form['number_input']
-        print(number_input)
+    else:
+        number_input = request.args.get('number_input')
 
     if request.method == 'POST':
         currency_input = request.form['currency_input']
-        print(currency_input)
+    else:
+        currency_input = request.args.get('currency_input')
+
+
+    if currency_input == 'usd':
+        converted_amount = float(number_input) * usd
+        print(converted_amount)
 
 
 
 
 
     return render_template('currency.html')
+
+
+
+
+
+
+
+
+
