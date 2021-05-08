22c698aa71a47fb95fb81a43dfc972524b21e63a
Leonard <66140015+Wardason@users.noreply.github.com>
2021-05-08 09:23:25 +0200

    finished password generator (python)

--- files/pwd_generator.py
+++ files/pwd_generator.py
@@ -1,4 +1,5 @@
 from flask import Flask, Blueprint, render_template, request
+import base64
 import os
 
 pwd_generator = Blueprint('pwd_generator', __name__)
@@ -6,4 +7,13 @@ pwd_generator = Blueprint('pwd_generator', __name__)
 
 @pwd_generator.route('/password', methods=['GET', 'POST'])
 def generator():
-    return render_template('pwd_generator.html')
+    if request.method == 'POST':
+        number_input = request.form['number_input']
+    else:
+        number_input = request.args.get('number_input')
+
+    length = int(number_input)
+    password = base64.b64encode(os.urandom(length)).decode("ascii")
+    msg = f'Your password is {password}'
+
+    return render_template('pwd_generator.html', variable=msg)
